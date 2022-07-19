121fb7e2bc348ad8646d829ed7c219fd888fc368
Aycarambo <stan.lucas07@gmail.com>
2022-07-19 19:04:35 +0200

    Base application showing every posts, with their respective title, body and user's name

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,102 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
+</script>
+
+<script>
+export default {
+  data() {
+    return { posts: [], users: [] };
+  },
+  created() {
+    /*fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => response.json())
+      .then((json) => console.log(json));*/
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => {
+        this.posts = json;
+        console.log(this.posts);
+      });
+    /*fetch("https://jsonplaceholder.typicode.com/comments")
+      .then((response) => response.json())
+      .then((json) => console.log(json));*/
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((json) => {
+        this.users = json;
+        console.log(this.users);
+      });
+    /* fetch("https://jsonplaceholder.typicode.com/albums")
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+    fetch("https://jsonplaceholder.typicode.com/photos")
+      .then((response) => response.json())
+      .then((json) => console.log(json));*/
+  },
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <main>
+    <div class="post" v-for="post in posts">
+      <div class="post-user">{{ users[post.userId - 1].name }}</div>
+      <div class="post-content">
+        <strong class="post-title">{{ post.title }}</strong>
+        <p class="post-body">{{ post.body }}</p>
+      </div>
+    </div>
+  </main>
+  <!-- <header>
+    <h1>{{ data }}</h1>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="./assets/logo.svg"
+      width="125"
+      height="125"
+    />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld :msg="data" />
     </div>
   </header>
 
   <main>
     <TheWelcome />
-  </main>
+  </main> -->
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+strong {
+  font-weight: bold;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.post {
+  padding: 5px;
+  border: 1px solid white;
+  margin-bottom: 20px;
+  display: flex;
+}
+
+.post-user {
+  min-width: 100px;
+}
+
+.post-content {
+  display: flex;
+  flex-direction: column;
+}
+
+.post-title {
+  font-size: 30px;
+}
+
+.post-body {
+  font-size: 24px;
 }
 
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+  /* laptop specific styling */
 }
 </style>
